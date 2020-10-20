bdf647a103a734b93beb626eeda48ddc590824a8
chengyz <1205300817@qq.com>
2020-10-20 09:28:07 +0800

    1. 函数调用验证 2. 闭包/箭头函数测试

--- hello-demo/src/example/Hello3.js
+++ hello-demo/src/example/Hello3.js
@@ -7,22 +7,76 @@ export default class Clock extends React.Component {
         color: '#00f2ff'
     }
 
-    htmlArr=[
-        <h1>菜鸟教程</h1>,
-        <h2>学的不仅是技术，更是梦想！</h2>
+    htmlArr = [
+        <h1 key={1001}>菜鸟教程</h1>,
+        <h2 key={1002}>学的不仅是技术，更是梦想！</h2>
     ]
+
+    constructor(props) {
+        super(props);
+        console.log("函数调用：{}, 参数：{}", "constructor", props)
+        this.state = {date: new Date()}
+    }
+
+    componentDidMount() {
+        console.log("函数调用：{}", "componentDidMount")
+        this.timerID = setInterval(() => this.tick(), 1000)
+
+        this.closureTest()
+    }
+
+    componentWillUnmount() {
+        console.log("函数调用：{}", "componentWillUnmount")
+        clearInterval(this.timerID)
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
 
     render() {
         return (
             <div>
                 {/*此为注释*/}
                 <h1>Hello, world! ——Hello3</h1>
-                <h2>现在是 {this.props.date.toLocaleTimeString()}.</h2>
+                <h1>Hello, {this.props.name}</h1>
+                <h2>现在是 {this.state.date.toLocaleTimeString()}.</h2>
                 <h3>10+1 = {10 + 1}</h3>
                 <h3 style={this.styleHello3}>本次考试成绩：{this.props.score}，是否及格：{this.props.score > 60 ? '及格' : '不及格'}</h3>
                 <h3>1. 下面进行html 数组渲染，数组长度：{this.htmlArr.length}</h3>
                 <div>{this.htmlArr}</div>
             </div>
         );
+    }
+
+    // 标准的闭包函数
+    A() {
+        let i = 0;
+        return function b() {
+            return (++i);
+        };
+    };
+
+    closureTest() {
+        // 标准闭包
+        let a = this.A();
+        console.log("标准闭包测试------------")
+        console.log(a());
+        console.log(a());
+
+        // 箭头函数体闭包
+        let B = (i = 0) => () => (++i);
+        let b = B();
+        console.log("箭头函数体闭包测试------------")
+        console.log(b());
+        console.log(b());
+
+        // 箭头函数递归
+        let fact = (x) => (x == 0 ? 1 : x * fact(x - 1));
+        console.log("箭头函数递归测试------------")
+        console.log(fact(5));
+
     }
 }
--- hello-demo/src/index.js
+++ hello-demo/src/index.js
@@ -10,26 +10,44 @@ import Hello3 from './example/Hello3';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-function tick(){
+Hello3.defaultProps={
+    name : '中国人'
+}
+
+const tickElement =
+    <div>
+        <Hello/>
+        <hr/>
+        <Hello2 date={new Date()}/>
+        <hr/>
+        <Hello3 score={90}/>
+    </div>
+    /*严格模式*/
+    /*React在严格模式下会执行两次render以帮忙检查额外的副作用*/
+    // <React.StrictMode>
+    //     <Hello/>
+    //     <hr/>
+    //     <Hello2 date={new Date()}/>
+    //     <hr/>
+    //     <Hello3 date={new Date()} score={90}/>
+    // </React.StrictMode>
+
+function tick() {
     ReactDOM.render(
-        <React.StrictMode>
-            <Hello />
-            <hr />
-            <Hello2 date={new Date()} />
-            <hr />
-            <Hello3 date={new Date()} score={90} />
-        </React.StrictMode>,
+        tickElement,
         document.getElementById('example')
     );
 }
+
 // 定时更新界面，每秒钟调用一次 ReactDOM.render()。
-setInterval(tick, 1000);
+// setInterval(tick, 1000);
+tick()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
